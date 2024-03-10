c5dfa9e0148a5e513416e1b7807210cda8138d56
ferdousalam007 <iamferdousalam007@gmail.com>
2024-03-10 15:33:18 +0600

    font family all added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'inter': ['inter', 'sans-serif'],
+        'antonio': ['Antonio', 'sans-serif'],
+        'bebas': ['Bebas Neue', 'sans-serif'],
+        'syne': ['Syne', 'sans-serif'],
+        'helvetica': ['Helvetica Neue', 'sans-serif'],
+        'product': ['Product Sans', 'sans-serif'],
+        'heading': ['Heading Now Trial', 'sans-serif'],
+      },
+      fontWeight: {
+        "100": 100,
+        "200": 200,
+        "300": 300,
+        "400": 400,
+        "500": 500,
+        "600": 600,
+        "700": 700,
+        "800": 800,
+        "900": 900,
+      },
+    },
   },
   plugins: [],
 };
