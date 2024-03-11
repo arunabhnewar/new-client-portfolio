2792db2c1ac468b0d0bddfc6ef83bae9267420b5
ferdousalam007 <iamferdousalam007@gmail.com>
2024-03-11 20:03:44 +0600

    home experience end

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -27,7 +27,7 @@ export default function Home() {
           {/* banner top part for title end*/}
           {/* banner bottm part start*/}
           <div className="p-4 bg-bblue rounded-2xl md:flex md:gap-6 md:py-10 xl:pl-10 xl:pr-5">
-            <h2 className="font-inter font-800 text-2xl text-green mb-8 md:text-7xl xl:7xl">
+            <h2 className="font-inter font-800 text-2xl text-green mb-8 md:text-7xl xl:7xl ">
               Android Developer
             </h2>
             <p className="font-inter font-400 text-base text-black sm:text-xl md:text-2xl lg:text-3xl">
@@ -39,44 +39,92 @@ export default function Home() {
           {/* banner bottm part end*/}
           {/* project part start*/}
           <div className="mt-36">
-            <h2 className="font-inter font-800 text-5xl sm:text-[80px] text-green mb-16">Projects</h2>
+            <h2 className="font-inter font-800 text-5xl sm:text-[80px] text-green mb-16">
+              Projects
+            </h2>
             <div className="grid grid-cols-1  md:grid-cols-2 gap-7">
               <div className="bg-bpink rounded-[48px]  flex flex-col justify-center items-center">
                 <div className="flex flex-col justify-center items-center  p-10 ">
                   <div className="min-w-[282px]">
-                    <img className="w-full" src={projectOne} alt="Truth Or Dare" />
+                    <img
+                      className="w-full"
+                      src={projectOne}
+                      alt="Truth Or Dare"
+                    />
                   </div>
-                  <h3 className="font-inter font-500 text-2xl mt-10 mb-4 sm:text-[40px] ">Truth Or Dare</h3>
+                  <h3 className="font-inter font-500 text-2xl mt-10 mb-4 sm:text-[40px] ">
+                    Truth Or Dare
+                  </h3>
                 </div>
               </div>
-              <div className="bg-bpink rounded-[48px]  flex flex-col justify-center items-center">
+              <div className="bg-bred rounded-[48px]  flex flex-col justify-center items-center">
                 <div className="flex flex-col justify-center items-center  p-10 ">
                   <div className="min-w-[282px]">
-                    <img className="w-full" src={projectTwo} alt="Unexpectedly" />
+                    <img
+                      className="w-full"
+                      src={projectTwo}
+                      alt="Unexpectedly"
+                    />
                   </div>
-                  <h3 className="font-inter font-500 text-2xl mt-10 mb-4 sm:text-[40px] ">Unexpectedly</h3>
+                  <h3 className="font-inter font-500 text-2xl mt-10 mb-4 sm:text-[40px] ">
+                    Unexpectedly
+                  </h3>
                 </div>
               </div>
-              <div className="bg-bpink rounded-[48px]  flex flex-col justify-center items-center">
+              <div className="bg-bblue rounded-[48px]  flex flex-col justify-center items-center">
                 <div className="flex flex-col justify-center items-center  p-10 ">
                   <div className="min-w-[282px] max-w-[345px]">
-                    <img className="w-full" src={projectThree} alt="Level Shoes" />
+                    <img
+                      className="w-full"
+                      src={projectThree}
+                      alt="Level Shoes"
+                    />
                   </div>
-                  <h3 className="font-inter font-500 text-2xl mt-10 mb-4 sm:text-[40px] ">Level Shoes</h3>
+                  <h3 className="font-inter font-500 text-2xl mt-10 mb-4 sm:text-[40px] ">
+                    Level Shoes
+                  </h3>
                 </div>
               </div>
-           
             </div>
           </div>
           {/* project  part end*/}
-           {/* Experience Section start */}
-      <div className="mt-36">
-      <h2 className="font-inter font-800 text-5xl sm:text-[80px] text-green mb-16">Projects</h2>
-      </div>
-        {/* Experience Section end */}
+          {/* Experience Section start */}
+          <div className="mt-36">
+            <h2 className="font-inter font-800 text-5xl sm:text-[80px] text-green mb-16 ">
+              Experience
+            </h2>
+            <div className="flex flex-col sm:flex-row   border-b border-black">
+              <div className="w-full font-inter font-800 text-3xl mb-8 sm:w-full md:w-1/3 ">Android Engineer</div>
+              <div className="w-full sm:w-full md:w-1/2 flex-grow">
+                <div className="flex items-center sm:flex-row sm:justify-end gap-6 mb-6">
+                  <p className="text-base bg-bblue rounded-2xl px-3 py-3">2023 - Present</p>
+                  <p className="text-base">Jaguar LandRover</p>
+                </div>
+              </div>
+            </div>
+            
+            <div className="flex flex-col sm:flex-row  border-b border-black mt-6">
+              <div className="w-full font-inter font-800 text-3xl mb-8 sm:w-full md:w-1/3 ">Android Engineer</div>
+              <div className="w-full sm:w-full md:w-1/2 flex-grow mb-6">
+                <div className="flex items-center sm:flex-row gap-6 sm:justify-end">
+                  <p className="text-base bg-bblue rounded-2xl px-3 py-3">2023 - Present</p>
+                  <p className="text-base">Jaguar LandRover</p>
+                </div>
+              </div>
+            </div>
+            
+            <div className="flex flex-col sm:flex-row mb-2 mt-6">
+              <div className="w-full font-inter font-800 text-3xl mb-8 sm:w-full md:w-1/3 ">Android Engineer</div>
+              <div className="w-full sm:w-full md:w-1/2 flex-grow">
+                <div className="flex items-center sm:flex-row gap-6 sm:justify-end">
+                  <p className="text-base bg-bblue rounded-2xl px-3 py-3">2023 - Present</p>
+                  <p className="text-base">Jaguar LandRover</p>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Experience Section end */}
         </div>
-
-       
       </div>
     </div>
   );
