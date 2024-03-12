0f27f8f3cbca66d31d1901936a86a7f0f0a29417
arunabhnewar <newardevil@gmail.com>
2024-03-12 10:48:19 +0600

    about me page content add

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -1,4 +1,6 @@
+import { Link } from "react-router-dom";
 import PageTitle from "../Components/Shared/PageTitle/PageTitle";
+import bigArrow from "../assets/icons/bigArrow.svg";
 import authorMid from "../assets/images/authorMid.webp";
 
 export default function AboutMe() {
@@ -30,10 +32,10 @@ export default function AboutMe() {
 
         {/* Work Experience Section start */}
         <div className='container max-w-1000 w-full mx-auto mt-[176px] px-6'>
-          <h1 className='font-heading font-700 text-[84px] text-green tracking-normal '>
+          <h1 className='font-heading font-700 text-[84px]  text-green tracking-normal '>
             Work Experience
           </h1>
-          <p className=' text-dark text-3xl font-400 tracking-wide w-[900px] leading-[34px] '>
+          <p className=' text-dark  text-3xl  font-400 tracking-wide  w-[900px]  leading-[34px] '>
             My portfolio showcases several professional Android apps available
             on the Google Play Store, that demonstrate my expertise in utilising
             a wide range of tools and technologies.
@@ -43,7 +45,7 @@ export default function AboutMe() {
 
         {/* Experience Section start */}
         <div className='container max-w-1000 w-full mx-auto mt-[166px] px-6'>
-          {/* Android developer experience */}
+          {/* Android developer experience start */}
           <div>
             <div className='flex justify-between items-center'>
               <h4 className='font-heading font-500 text-black text-5xl  '>
@@ -57,8 +59,9 @@ export default function AboutMe() {
 
             <hr className='mt-7 border-black' />
           </div>
+          {/* Android developer experience end */}
 
-          {/* Contract Android developer experience */}
+          {/* Contract Android developer experience start */}
           <div className='mt-7'>
             <div className='flex justify-between items-center'>
               <h4 className='font-heading font-500 text-black text-5xl  '>
@@ -72,8 +75,97 @@ export default function AboutMe() {
 
             <hr className='mt-7 border-black' />
           </div>
+          {/* Contract Android developer experience end */}
+
+          {/* Freelance Developer experience start */}
+          <div className='mt-7'>
+            <div className='flex justify-between items-center'>
+              <h4 className='font-heading font-500 text-black text-5xl  '>
+                Freelance Developer
+              </h4>
+
+              <p className='text-right text-green font-400 text-xl '>
+                Freelance <br /> Nov 2021 - Sept 2022
+              </p>
+            </div>
+
+            <hr className='mt-7 border-black' />
+          </div>
+          {/* Freelance Developer  experience end */}
         </div>
         {/* Experience Section end */}
+
+        {/* Lets Talk section start */}
+        <div className='container max-w-1000 w-full mx-auto mt-[250px] px-6'>
+          <Link to='mailto:akshit6302@gmail.com'>
+            <p className='font-400 font-product leading-8 text-2xl tracking-wide text-green  '>
+              akshitemail@mail.com
+            </p>
+          </Link>
+
+          <div className='flex justify-between items-end'>
+            <h1 className='uppercase font-heading font-400 text-dark text-8xl  '>
+              Let’s Talk
+            </h1>
+            <div className='w-10 h-10   '>
+              <img src={bigArrow} alt='' className='w-full h-full' />
+            </div>
+          </div>
+        </div>
+        {/* Lets Talk section end */}
+
+        {/* Footer Start */}
+        <div className='container max-w-1000 w-full mx-auto mt-[124px] px-6 pb-10'>
+          <div className='flex justify-between items-center'>
+            {/* email content start */}
+            <div className='font-helvetica font-400 text-lg leading-[21.47px] text-dark '>
+              <p>Hey get in touch</p>
+              <p>akshit6302@gmail.com</p>
+            </div>
+            {/* email content start */}
+
+            {/* social media content start */}
+            <div>
+              <p className='font-700 font-helvetica text-lg leading-[21.47px] text-dark'>
+                <Link
+                  to='https://www.instagram.com/ak__it/?hl=en'
+                  target='_blank'
+                  className='underline'>
+                  Instagram
+                </Link>
+                ,{" "}
+                <Link
+                  to='https://www.linkedin.com/in/akshit-nahata-06948a13b/'
+                  target='_blank'
+                  className='underline'>
+                  LinkedIn
+                </Link>
+              </p>
+
+              <p className='font-700 font-helvetica text-lg leading-[21.47px] text-dark'>
+                <Link
+                  to='https://www.buymeacoffee.com/akshit6302'
+                  target='_blank'
+                  className='underline'>
+                  BuyCoffee
+                </Link>
+              </p>
+            </div>
+            {/* social media content end */}
+
+            {/* copyright content start */}
+            <div className='text-right font-helvetica font-400 text-lg leading-[21.47px] text-dark'>
+              <p>Designed by </p>
+              <p>
+                <Link to='https://www.figma.com/@ericandtheweb' target='_blank'>
+                  © ericandtheweb
+                </Link>
+              </p>
+            </div>
+            {/* copyright content end */}
+          </div>
+        </div>
+        {/* Footer end */}
       </div>
     </>
   );
